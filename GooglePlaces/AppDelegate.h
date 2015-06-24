c0126dbcafe895f50957d2166d506b9aa9d9ad45
Daniel Trostli <daniel.trostli@gmail.com>
2015-06-24 12:15:40 -0700

    Initial Commit

---
+++ GooglePlaces/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GooglePlaces
+//
+//  Created by Daniel Trostli on 6/24/15.
+//  Copyright (c) 2015 Daniel Trostli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GooglePlaces/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GooglePlaces
+//
+//  Created by Daniel Trostli on 6/24/15.
+//  Copyright (c) 2015 Daniel Trostli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
